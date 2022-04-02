3e921c435e76d9cd45c47c6348c9a2254dcb58dd
Thresa Kelly <polaris133pc@gmail.com>
2022-04-02 13:41:13 -0500

    finished all boxes for student info

--- SRMS/StudentUI.Designer.cs
+++ SRMS/StudentUI.Designer.cs
@@ -30,13 +30,25 @@
         {
             this.tabControl_studentInfo = new System.Windows.Forms.TabControl();
             this.tabPage_welcome = new System.Windows.Forms.TabPage();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label_classes = new System.Windows.Forms.Label();
+            this.label_accInfo = new System.Windows.Forms.Label();
+            this.label_welcome = new System.Windows.Forms.Label();
             this.tabPage_accInfo = new System.Windows.Forms.TabPage();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_lastName = new System.Windows.Forms.TextBox();
+            this.textBox_firstName = new System.Windows.Forms.TextBox();
+            this.label_id = new System.Windows.Forms.Label();
+            this.label_lastName = new System.Windows.Forms.Label();
+            this.label_firstName = new System.Windows.Forms.Label();
             this.tabPage_classes = new System.Windows.Forms.TabPage();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.label_email = new System.Windows.Forms.Label();
+            this.textBox_major = new System.Windows.Forms.TextBox();
+            this.label_major = new System.Windows.Forms.Label();
             this.tabControl_studentInfo.SuspendLayout();
             this.tabPage_welcome.SuspendLayout();
+            this.tabPage_accInfo.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl_studentInfo
@@ -47,61 +59,129 @@
             this.tabControl_studentInfo.Controls.Add(this.tabPage_welcome);
             this.tabControl_studentInfo.Controls.Add(this.tabPage_accInfo);
             this.tabControl_studentInfo.Controls.Add(this.tabPage_classes);
-            this.tabControl_studentInfo.Location = new System.Drawing.Point(221, 63);
+            this.tabControl_studentInfo.Location = new System.Drawing.Point(197, 63);
             this.tabControl_studentInfo.Name = "tabControl_studentInfo";
             this.tabControl_studentInfo.SelectedIndex = 0;
-            this.tabControl_studentInfo.Size = new System.Drawing.Size(598, 378);
+            this.tabControl_studentInfo.Size = new System.Drawing.Size(622, 378);
             this.tabControl_studentInfo.TabIndex = 0;
             // 
             // tabPage_welcome
             // 
-            this.tabPage_welcome.Controls.Add(this.label2);
-            this.tabPage_welcome.Controls.Add(this.label1);
+            this.tabPage_welcome.Controls.Add(this.label_classes);
+            this.tabPage_welcome.Controls.Add(this.label_accInfo);
+            this.tabPage_welcome.Controls.Add(this.label_welcome);
             this.tabPage_welcome.Location = new System.Drawing.Point(4, 25);
             this.tabPage_welcome.Name = "tabPage_welcome";
-            this.tabPage_welcome.Size = new System.Drawing.Size(590, 349);
+            this.tabPage_welcome.Size = new System.Drawing.Size(614, 349);
             this.tabPage_welcome.TabIndex = 2;
             this.tabPage_welcome.Text = "Welcome";
             this.tabPage_welcome.UseVisualStyleBackColor = true;
             this.tabPage_welcome.Click += new System.EventHandler(this.tabPage_welcome_Click);
             // 
-            // label2
+            // label_classes
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 133);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(510, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Select the \"Account Information\" tab above to view and edit your personal informa" +
+            this.label_classes.AutoSize = true;
+            this.label_classes.Location = new System.Drawing.Point(51, 175);
+            this.label_classes.Name = "label_classes";
+            this.label_classes.Size = new System.Drawing.Size(403, 16);
+            this.label_classes.TabIndex = 2;
+            this.label_classes.Text = "Select the \"Classes\" tab to view your enrolled classes and grades. ";
+            // 
+            // label_accInfo
+            // 
+            this.label_accInfo.AutoSize = true;
+            this.label_accInfo.Location = new System.Drawing.Point(51, 133);
+            this.label_accInfo.Name = "label_accInfo";
+            this.label_accInfo.Size = new System.Drawing.Size(510, 16);
+            this.label_accInfo.TabIndex = 1;
+            this.label_accInfo.Text = "Select the \"Account Information\" tab above to view and edit your personal informa" +
     "tion.";
             // 
-            // label1
+            // label_welcome
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(47, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(170, 38);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome!";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label_welcome.AutoSize = true;
+            this.label_welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_welcome.Location = new System.Drawing.Point(47, 52);
+            this.label_welcome.Name = "label_welcome";
+            this.label_welcome.Size = new System.Drawing.Size(170, 38);
+            this.label_welcome.TabIndex = 0;
+            this.label_welcome.Text = "Welcome!";
+            this.label_welcome.Click += new System.EventHandler(this.label1_Click);
             // 
             // tabPage_accInfo
             // 
+            this.tabPage_accInfo.Controls.Add(this.textBox_major);
+            this.tabPage_accInfo.Controls.Add(this.label_major);
+            this.tabPage_accInfo.Controls.Add(this.textBox_email);
+            this.tabPage_accInfo.Controls.Add(this.label_email);
+            this.tabPage_accInfo.Controls.Add(this.textBox_id);
+            this.tabPage_accInfo.Controls.Add(this.textBox_lastName);
+            this.tabPage_accInfo.Controls.Add(this.textBox_firstName);
+            this.tabPage_accInfo.Controls.Add(this.label_id);
+            this.tabPage_accInfo.Controls.Add(this.label_lastName);
+            this.tabPage_accInfo.Controls.Add(this.label_firstName);
             this.tabPage_accInfo.Location = new System.Drawing.Point(4, 25);
             this.tabPage_accInfo.Name = "tabPage_accInfo";
             this.tabPage_accInfo.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_accInfo.Size = new System.Drawing.Size(590, 349);
+            this.tabPage_accInfo.Size = new System.Drawing.Size(614, 349);
             this.tabPage_accInfo.TabIndex = 0;
             this.tabPage_accInfo.Text = "Account Information";
             this.tabPage_accInfo.UseVisualStyleBackColor = true;
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(118, 98);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(100, 22);
+            this.textBox_id.TabIndex = 5;
+            // 
+            // textBox_lastName
+            // 
+            this.textBox_lastName.Location = new System.Drawing.Point(118, 70);
+            this.textBox_lastName.Name = "textBox_lastName";
+            this.textBox_lastName.Size = new System.Drawing.Size(100, 22);
+            this.textBox_lastName.TabIndex = 4;
+            // 
+            // textBox_firstName
+            // 
+            this.textBox_firstName.Location = new System.Drawing.Point(118, 41);
+            this.textBox_firstName.Name = "textBox_firstName";
+            this.textBox_firstName.Size = new System.Drawing.Size(100, 22);
+            this.textBox_firstName.TabIndex = 3;
+            // 
+            // label_id
+            // 
+            this.label_id.AutoSize = true;
+            this.label_id.Location = new System.Drawing.Point(40, 101);
+            this.label_id.Name = "label_id";
+            this.label_id.Size = new System.Drawing.Size(68, 16);
+            this.label_id.TabIndex = 2;
+            this.label_id.Text = "Student ID";
+            // 
+            // label_lastName
+            // 
+            this.label_lastName.AutoSize = true;
+            this.label_lastName.Location = new System.Drawing.Point(40, 73);
+            this.label_lastName.Name = "label_lastName";
+            this.label_lastName.Size = new System.Drawing.Size(72, 16);
+            this.label_lastName.TabIndex = 1;
+            this.label_lastName.Text = "Last Name";
+            // 
+            // label_firstName
+            // 
+            this.label_firstName.AutoSize = true;
+            this.label_firstName.Location = new System.Drawing.Point(40, 44);
+            this.label_firstName.Name = "label_firstName";
+            this.label_firstName.Size = new System.Drawing.Size(72, 16);
+            this.label_firstName.TabIndex = 0;
+            this.label_firstName.Text = "First Name";
             // 
             // tabPage_classes
             // 
             this.tabPage_classes.Location = new System.Drawing.Point(4, 25);
             this.tabPage_classes.Name = "tabPage_classes";
             this.tabPage_classes.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_classes.Size = new System.Drawing.Size(590, 349);
+            this.tabPage_classes.Size = new System.Drawing.Size(614, 349);
             this.tabPage_classes.TabIndex = 1;
             this.tabPage_classes.Text = "Classes";
             this.tabPage_classes.UseVisualStyleBackColor = true;
@@ -112,6 +192,38 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 2;
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(118, 126);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(100, 22);
+            this.textBox_email.TabIndex = 7;
+            // 
+            // label_email
+            // 
+            this.label_email.AutoSize = true;
+            this.label_email.Location = new System.Drawing.Point(40, 129);
+            this.label_email.Name = "label_email";
+            this.label_email.Size = new System.Drawing.Size(41, 16);
+            this.label_email.TabIndex = 6;
+            this.label_email.Text = "Email";
+            // 
+            // textBox_major
+            // 
+            this.textBox_major.Location = new System.Drawing.Point(118, 154);
+            this.textBox_major.Name = "textBox_major";
+            this.textBox_major.Size = new System.Drawing.Size(100, 22);
+            this.textBox_major.TabIndex = 9;
+            // 
+            // label_major
+            // 
+            this.label_major.AutoSize = true;
+            this.label_major.Location = new System.Drawing.Point(40, 157);
+            this.label_major.Name = "label_major";
+            this.label_major.Size = new System.Drawing.Size(41, 16);
+            this.label_major.TabIndex = 8;
+            this.label_major.Text = "Major";
             // 
             // StudentUI
             // 
@@ -127,6 +239,8 @@
             this.tabControl_studentInfo.ResumeLayout(false);
             this.tabPage_welcome.ResumeLayout(false);
             this.tabPage_welcome.PerformLayout();
+            this.tabPage_accInfo.ResumeLayout(false);
+            this.tabPage_accInfo.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,8 +252,19 @@
         private System.Windows.Forms.TabPage tabPage_accInfo;
         private System.Windows.Forms.TabPage tabPage_classes;
         private System.Windows.Forms.TabPage tabPage_welcome;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_welcome;
+        private System.Windows.Forms.Label label_accInfo;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_classes;
+        private System.Windows.Forms.Label label_firstName;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.TextBox textBox_lastName;
+        private System.Windows.Forms.TextBox textBox_firstName;
+        private System.Windows.Forms.Label label_id;
+        private System.Windows.Forms.Label label_lastName;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.Label label_email;
+        private System.Windows.Forms.TextBox textBox_major;
+        private System.Windows.Forms.Label label_major;
     }
 }
