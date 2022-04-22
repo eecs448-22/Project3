7788a7721cf104e624d161a5cc0f7a500b06d395
mswartz9 <maggieswartz@gmail.com>
2022-04-22 09:30:34 -0500

    Updated test suite desc. + dummy test

--- TestSuite/TestSuite.cs
+++ TestSuite/TestSuite.cs
@@ -1,6 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using DBMS;
 
 namespace TestSuite
 {
@@ -8,9 +7,20 @@ namespace TestSuite
     public class TestSuite
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Admin_Create_New_User()
         {
-            private string connString = SqliteDataAccess.LoadConnectionString;
+            //Steps needed:
+            //Make a new user profile to enter in database
+            //Enter that user into the database
+            //Check the database to verify the user is there
+            //If yes, return test passed, if no, return pass failed.
+
+            //Below is a dummy test so you can see how the test explorer will work
+            //Go to Test->TestExplorer->Run All Tests, and you'll see the test run and the outcome (pass)
+            
+            double num1 = 4.5;
+            double num2 = 4.5;
+            Assert.AreEqual(num1, num2);
 
         }
     }
